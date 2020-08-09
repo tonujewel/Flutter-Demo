f4c11c5df70c9271f2dab81876fc74d374e5fb51
Jewel Rana <tonujewel@gmail.com>
2020-08-09 13:07:12 +0600

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,42 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Exploring UI widgets",
-    home: Home(),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Basic list view"),
+      ),
+      body: getListView(),
+    ),
   ));
+}
+
+Widget getListView() {
+  var listView = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful view"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: (){
+          debugPrint("Landscape tapped");
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_mac),
+        title: Text("Mac Machine"),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Phone"),
+      ),
+      Text("Another element in list"),
+      Container(
+        color: Colors.red,
+        height: 50.0,
+      )
+    ],
+  );
+
+  return listView;
 }
