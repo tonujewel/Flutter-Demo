308076ff35446e98c8af5bab86da09d4d828d5a2
Jewel Rana <tonujewel@gmail.com>
2020-08-09 22:13:48 +0600

    text field

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,57 @@
-import "package:flutter/material.dart";
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Exploring UI widgets",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Long list"),
-      ),
-      body: getListView(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          debugPrint("Floating button clicked");
-        },
-        child: Icon(Icons.add),
-        tooltip: "Add one more item",
-      ),
-    ),
+    title: "Stateful App Widget",
+    home: FavoriteCity(),
   ));
 }
 
-void showSnackBar(BuildContext context, String item) {
-  var snackBar = SnackBar(
-    content: Text(
-      "You tapped on $item",
-      style: TextStyle(
-          color: Colors.blue,
-          fontFamily: 'Roboto',
-          fontStyle: FontStyle.normal),
-    ),
-    action: SnackBarAction(
-        label: "UNDO",
-        onPressed: () {
-          debugPrint("clicked on UNDO");
-        }),
-  );
-  Scaffold.of(context).showSnackBar(snackBar);
+class FavoriteCity extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FavoriteCityState();
+  }
 }
 
-List<String> getListElement() {
-  var items = List<String>.generate(1000, (counter) => "Item $counter");
-  return items;
-}
+class _FavoriteCityState extends State<FavoriteCity> {
+  String nameCity = "";
 
-Widget getListView() {
-  var listItems = getListElement();
-  var listView = ListView.builder(itemBuilder: (contex, index) {
-    return ListTile(
-      leading: Icon(Icons.adjust),
-      title: Text(listItems[index]),
-      onTap: () {
-        showSnackBar(contex, listItems[index]);
-      },
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful App Example"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: "Your City Name",
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.blue, width: 2.0),
+                      borderRadius: BorderRadius.circular(25.0))),
+              onChanged: (String userInput) {
+                setState(() {
+                  nameCity = userInput;
+                });
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text(
+                "Your Best City Is: $nameCity ",
+                style: TextStyle(fontSize: 20.0),
+              ),
+            )
+          ],
+        ),
+      ),
     );
-  });
-
-  return listView;
+  }
 }
